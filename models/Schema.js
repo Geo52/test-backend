fbbcc804dc36735d594c287a4a6c3d4358d9fec6
Jorge Rios <jorgeluisrios47@gmail.com>
2024-01-15 11:40:20 -0600

    first commit

---
+++ models/Schema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+
+const BlogPost = new Schema({
+  title: String,
+});
+
+module.exports = mongoose.model('Schema', BlogPost)
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+const Schema = require("./models/Schema");
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', async (req, res) => {
+  try {
+    // Assuming your schema has fields like 'name', 'email', etc. Adjust as needed.
+    const newData = new Schema({
+      title: req.body.title
+      // Add other fields as needed
+    });
+
+    const savedData = await newData.save();
+    res.json(savedData);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
+
+mongoose
+  .connect(
+    "mongodb+srv://jorge:69JnidkcqhIXUwQn@test.amni2ct.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  });
